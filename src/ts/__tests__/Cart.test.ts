e1bd37af64a118e38903c041d1d30d1894192119
Mkrtychiyants <heisaloosershesadgmail@gmail.com>
2023-04-28 12:36:09 +0300

    second commit

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,15 @@
 import Cart from '../service/Cart';
+import MusicAlbum from '../domain/MusicAlbum';
+import Movie from '../domain/Movie';
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+test('card should not be empty', () => {
+  const cart = new Cart();
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  cart.add(new Movie(111,'Avengers','Мстители',190,2012,'USA','Avengers Assemble','action, fantasy, adventures', 137));
+  expect(cart.items.length).toBe(2);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,26 +6,25 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
+  },
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
-      {
-        test: /\.txt$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'ts-loader',
         },
-
       },
       {
         test: /\.js$/,
-        loader: 'raw-loader',
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
@@ -41,8 +40,13 @@ module.exports = {
       template: './src/index.html',
       filename: 'main.html',
     }),
+    new MiniCSSExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+
 };
